a8c2ffb662eea51b122bc0405fbe101ae59c2fdb
AlexeySevalnikov <Scorpion9922>
2021-02-15 21:36:01 +0300

    Test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,91 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String numberPhone = "89156078352";
+        String numberPhone = "+7 (915) 607-83-52";//89156078352  +7 915 607 83-52
         System.out.println("Введенный номер телефона: " + numberPhone);
-        System.out.println("Скорректированный номер телефона: " + correctPhone2(numberPhone)); // сделал 2 метода correctPhone1 чисто на if и correctPhone2 с циклом покороче
+        System.out.println("Скорректированный номер телефона: " + correctPhone1(numberPhone));
     }
 
     public static String correctPhone1(String phone) {
-        if (phone.length() > 11) {
-            if (phone.contains("+7")) {
-                phone = phone.replace("+7", "8");
-                System.out.print("Замена +7 на 8");
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
-                }
-            }
-            if (phone.contains("(")) {
-                phone = phone.replace("(", "");
-                System.out.print("Удаление открывающейся скобки '('");
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
-                }
-            }
-            if (phone.contains(")")) {
-                phone = phone.replace(")", "");
-                System.out.print("Удаление закрывающейся скобки ')'");
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
-                }
-            }
-            if (phone.contains("-")) {
-                phone = phone.replace("-", "");
-                System.out.print("Удаление дефиса '-' ");
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
-                }
-            }
-            if (phone.contains(" ")) {
-                phone = phone.replace(" ", "");
-                System.out.print("Удаление пробелов");
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
-                }
-            }
-            if (phone.length() > 11) {
-                System.out.println("Веден некорректный номер телефона");
-            }
-
-        }
-        return phone;
-    }
-
-    public static String correctPhone2(String phone) {
         String[] massSymbol = {"+7", "(", ")", " ", "-"};
-        for (int i = 0; i < massSymbol.length; i++) {
-            if (phone.length() > 11) {
-                if (i == 0) {
-                    phone = phone.replace(massSymbol[i], "8");
-                    System.out.print("Замена +7 на 8");
-                } else {
-                    phone = phone.replace(massSymbol[i], "");
-                    System.out.print("Удаление " + "'" + massSymbol[i] + "'");
-                }
-            } else if (phone.length() == 11) {
-                System.out.println("Изменений не было.");
-                break;
-            }
-            if (phone.length() > 11) {
-                System.out.print("; ");
-            } else {
-                System.out.println(".");
-            }
-        }
         if (phone.length() > 11) {
-            System.out.println();
-            phone = "Веден некорректный номер телефона";
+            for (int i = 0; i < massSymbol.length; i++) {
+                if (i == 0) {
+                    if (phone.contains(massSymbol[i]) == false) {
+                        continue;
+                    } else {
+                        phone = phone.replace(massSymbol[i], "8");
+                        System.out.print("Замена +7 на 8");
+                    }
+                } else {
+                    if (phone.contains(massSymbol[i]) == false) {
+                        continue;
+                    } else {
+                        phone = phone.replace(massSymbol[i], "");
+                        System.out.print("Удаление " + "'" + massSymbol[i] + "'");
+                    }
+                }
+                if (phone.length() > 11) {
+                    System.out.print("; ");
+                } else {
+                    System.out.println(".");
+                }
+            }
+            if (phone.length() > 11) {
+                phone = "Веден некорректный номер телефона";
+            }
+        } else if (phone.length() == 11) {
+            phone = "Изменений не было.";
         }
+
         return phone;
     }
 }
