290c43f6abece636c26bb6730e3d877b5b28fdb7
AlexeySevalnikov <Scorpion9922>
2021-02-23 23:55:49 +0300

    New mass two Sting[]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,41 +5,51 @@ public class Main {
     public static void main(String[] args) {
         String numberPhone = "+7 (915) 607-83-52";//89156078352  +7 915 607 83-52
         System.out.println("Введенный номер телефона: " + numberPhone);
-        System.out.println("Скорректированный номер телефона: " + correctPhone1(numberPhone));
+        System.out.print("Скорректированный номер телефона: ");
+        String[] array;
+        array = correctPhone1(numberPhone);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 
-    public static String correctPhone1(String phone) {
+    public static String[] correctPhone1(String phone) {
         String[] massSymbol = {"+7", "(", ")", " ", "-"};
+        String correctOperation = "Замена +7 на 8; Удаление '('; Удаление ')'; Удаление ' '; Удаление '-'.";
+        String[] massStr={"",""};
         if (phone.length() > 11) {
             for (int i = 0; i < massSymbol.length; i++) {
                 if (i == 0) {
                     if (phone.contains(massSymbol[i]) == false) {
+                        correctOperation = correctOperation.replace("Замена +7 на 8;", "");
                         continue;
                     } else {
                         phone = phone.replace(massSymbol[i], "8");
-                        System.out.print("Замена +7 на 8");
                     }
                 } else {
-                    if (phone.contains(massSymbol[i]) == false) {
+                    if (phone.contains(massSymbol[i]) == false && correctOperation.contains(massSymbol[i])) {
+                        correctOperation = correctOperation.replace("; Удаление '" + massSymbol[i] + "'", "");
                         continue;
                     } else {
                         phone = phone.replace(massSymbol[i], "");
-                        System.out.print("Удаление " + "'" + massSymbol[i] + "'");
                     }
-                }
-                if (phone.length() > 11) {
-                    System.out.print("; ");
-                } else {
-                    System.out.println(".");
                 }
             }
             if (phone.length() > 11) {
                 phone = "Веден некорректный номер телефона";
+                correctOperation = "";
             }
         } else if (phone.length() == 11) {
             phone = "Изменений не было.";
+            correctOperation = "";
         }
-
-        return phone;
+        for (int i = 0; i < massStr.length; i++) {
+            if (i == 0) {
+                massStr[i] = phone;
+            } else {
+                massStr[i] = correctOperation;
+            }
+        }
+        return massStr;
     }
 }
